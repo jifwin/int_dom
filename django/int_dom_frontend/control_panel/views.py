169894d1e1056d6a3cf5ee45df2f577d90f64777
jifwin <gpietrusza@gmail.com>
2014-12-20 00:34:40 +0100

    template in view changes

--- django/int_dom_frontend/control_panel/views.py
+++ django/int_dom_frontend/control_panel/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    body_text = "Hello World!"
+    template = loader.get_template('control_panel/index.html')
+    context = RequestContext(request,
+        {
+
+            'body_text': body_text
+
+        })
+    return HttpResponse(template.render(context))
