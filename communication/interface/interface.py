696e1208fd095dc91d24200fd75cd6769ef42e73
jifwin <gpietrusza@gmail.com>
2014-12-25 14:40:49 +0100

    bluetooth interface

---
+++ communication/interface/interface.py
@@ -0,0 +1,20 @@
+__author__ = 'grzegorz'
+
+class Interface:
+
+    response = ""
+
+
+    def ConfigureBluetooth(self):
+        pass
+
+    def TestBluetooth(self): #testowanie polega na wyslaniu komendy danej komendy i oczewkiwaniu na jej potwierdzenie
+        pass
+
+
+    def SendMessage(self):
+        pass
+
+    def ReadResponse(self):
+        pass
+
