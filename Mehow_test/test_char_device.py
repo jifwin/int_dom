ee2184be75e740989c1c279ffb96fbce38036453
Mehow <mehow2122@gmail.com>
2015-01-06 00:44:15 +0100

    Repair again..

--- Mehow_test/test_char_device.py
+++ Mehow_test/test_char_device.py
@@ -2,11 +2,7 @@
 filename='pseudo_char_device'
 def FRDM_RESPONSE():
     value=[]
-<<<<<<< HEAD
     file_object = open(filename, 'r')
-=======
-    file_object = open('/dev/rfcomm0', 'r')
->>>>>>> 1d5133316cdeb933b4c1c4b2a2c227943ca51570
     response = file_object.readline()
     file_object.close()
     #print response
@@ -17,7 +13,6 @@ def FRDM_RESPONSE():
             value.append(i)
     return value
 def FRDM_SEND(value):
-<<<<<<< HEAD
     file_object = open(filename, 'w')
     value=value+"\n"                            #adding EOL
     file_object.write(value)
@@ -26,19 +21,13 @@ def REFRESH():
     file_object = open(filename, 'w')
     file_object.write(chr(128)+'\n')              #0b10000000 as refresh Question
     file_object.close()
-=======
-    file_object = open('/dev/rfcomm0', 'w')
->>>>>>> 1d5133316cdeb933b4c1c4b2a2c227943ca51570
 
 
 #TESTS:
 
 
-<<<<<<< HEAD
 #FRDM_SEND("AA")
 REFRESH()
-=======
-#TESTS:
-FRDM_SEND("A")
->>>>>>> 1d5133316cdeb933b4c1c4b2a2c227943ca51570
+#FRDM_SEND("A")
+
 print FRDM_RESPONSE()
