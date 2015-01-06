963357dd94c6eb648eea654f0965bb53809a3b66
jifwin <gpietrusza@gmail.com>
2015-01-06 04:09:30 +0100

    moze zadziala

---
+++ frdm/uart/Objects/sonar.d
@@ -0,0 +1,10 @@
+.\objects\sonar.o: sonar.c
+.\objects\sonar.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\MKL46Z4.h
+.\objects\sonar.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm0plus.h
+.\objects\sonar.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\sonar.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
+.\objects\sonar.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
+.\objects\sonar.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\system_MKL46Z4.h
+.\objects\sonar.o: sonar.h
+.\objects\sonar.o: tpm.h
+.\objects\sonar.o: sLCD.h
---
+++ frdm/uart/Objects/tpm.d
@@ -0,0 +1,11 @@
+.\objects\tpm.o: tpm.c
+.\objects\tpm.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\MKL46Z4.h
+.\objects\tpm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cm0plus.h
+.\objects\tpm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\tpm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmInstr.h
+.\objects\tpm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.2.0\CMSIS\Include\core_cmFunc.h
+.\objects\tpm.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.3.0\Device\Include\system_MKL46Z4.h
+.\objects\tpm.o: tpm.h
+.\objects\tpm.o: sonar.h
+.\objects\tpm.o: sLCD.h
+.\objects\tpm.o: ADC.h
