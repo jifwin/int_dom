6564e5209745781acda07fd3abf03b3fa769f300
jifwin <gpietrusza@gmail.com>
2015-01-06 04:02:37 +0100

    unpck

---
+++ frdm/uart/intdom.h
@@ -0,0 +1,11 @@
+#include "MKL46Z4.h"  
+
+#define NUM_OF_EVENTS 4
+#define ACTIONS_PER_PIN 4
+
+
+	
+	void configure_pins();
+	void handle_event(uint8_t value);
+	void send_refresh();
+	void send_ack();
