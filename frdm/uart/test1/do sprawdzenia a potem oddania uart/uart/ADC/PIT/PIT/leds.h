7565c6fe224529a1c6ab1697f9de35cc34fcab75
jifwin <gpietrusza@gmail.com>
2015-01-06 01:07:08 +0100

    remove uart 2

--- frdm/uart/test1/do sprawdzenia a potem oddania uart/uart/ADC/PIT/PIT/leds.h
+++
@@ -1,3 +0,0 @@
-#include "MKL46Z4.h"
-
-void ledsInitialize(void);
--- frdm/uart/test1/do sprawdzenia a potem oddania uart/uart/ADC/PIT/PIT/tpm.h
+++
@@ -1,7 +0,0 @@
-#include "MKL46Z4.h"
-#define TPM_Number TPM0_IRQn
-void tpm_init(void);
-
-void TPM1_IRQHandler(void);
-int adc_avg9(void);
-int pulse_width(void);
